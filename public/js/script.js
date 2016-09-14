624f498b17aef51689c4d99cbe6e82766d5301c3
Zach Gaines <zrgaines@mtu.edu>
2016-09-14 15:05:15 -0500

    added a couple sections

--- public/js/script.js
+++ public/js/script.js
@@ -6,5 +6,13 @@ $(document).on('scroll', function() {
       $('#tb2').addClass('zoomIn');
       $('#tb2').removeClass('disNone');
     }
+  if($(this).scrollTop()>=$('#tb3').position().top){
+      $('#tb3').addClass('zoomIn');
+      $('#tb3').removeClass('disNone');
+    }
+  if($(this).scrollTop()>=$('#tb4').position().top){
+      $('#tb4').addClass('zoomIn');
+      $('#tb4').removeClass('disNone');
+    }      
   })
 })
