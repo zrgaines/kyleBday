626666191c82ad68d5855349e80566ce93aafe17
Zach Gaines <zrgaines@mtu.edu>
2016-09-15 12:31:52 -0500

    5 text blocks

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,28 @@
 $(document).ready(function() {
   $('#tb1').addClass('zoomIn');
+
+  $("#button").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#myDiv").offset().top
+    }, 2000);
+  });
   
-$(document).on('scroll', function() {
-  if($(this).scrollTop()>=$('#tb2').position().top){
-      $('#tb2').addClass('zoomIn');
-      $('#tb2').removeClass('disNone');
-    }
-  if($(this).scrollTop()>=$('#tb3').position().top){
-      $('#tb3').addClass('zoomIn');
-      $('#tb3').removeClass('disNone');
-    }
-  if($(this).scrollTop()>=$('#tb4').position().top){
-      $('#tb4').addClass('zoomIn');
-      $('#tb4').removeClass('disNone');
-    }      
-  })
+  $(document).on('scroll', function() {
+    if($(this).scrollTop()>=$('#tb2').position().top){
+        $('#tb2').addClass('lightSpeedIn');
+        $('#tb2').removeClass('disNone');
+      }
+    if($(this).scrollTop()>=$('#tb3').position().top){
+        $('#tb3').addClass('fadeInLeftBig');
+        $('#tb3').removeClass('disNone');
+      }
+    if($(this).scrollTop()>=$('#tb4').position().top){
+        $('#tb4').addClass('flipInY');
+        $('#tb4').removeClass('disNone');
+      }
+    if($(this).scrollTop()>=$('#tb5').position().top){
+        $('#tb5').addClass('rotateInUpRight');
+        $('#tb5').removeClass('disNone');
+      }           
+    })
 })
